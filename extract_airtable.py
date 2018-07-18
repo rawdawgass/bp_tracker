68347bf193f47160f52697261a0df6a6e9c78050
Andy Do <andy@dnafund.onmicrosoft.com>
2018-07-18 14:41:30 -0700

    clean up a little

--- extract_airtable.py
+++ extract_airtable.py
@@ -11,9 +11,7 @@ bp_key_tbl = Airtable(config.base_key, config.bp_key, api_key=config.api_key)
 #print (bp_key_tbl.match('Name','eosfishrocks').get('id'))
 
 def get_airtable():
-
     bp_actions_df = json_normalize(bp_actions_tbl.get_all(view='Grid 2'))
-
     #rename cols remove airtables addition
     bp_actions_df.columns = [x.replace('fields.','') for x in bp_actions_df.columns]
     #bp_actions_df.to_csv('at_df.csv', index=False)
@@ -29,16 +27,11 @@ def add_uniques():
     #get new rows in the extract that are not already in the airtable
     common = bp_actions_df.merge(compile_df,on=['global_sequence'])
     new_actions_df = compile_df[(~compile_df.global_sequence.isin(common.global_sequence))]
-
     new_actions_df = new_actions_df.fillna('')
-
     new_rows = len(new_actions_df)
-    #normalize global_sequnce to int
-    #new_actions_df['bp_id'] = new_actions_df['bp_id'].apply(lambda row: bp_key_tbl.match('Name','eosfishrocks').get(row))
-    #new_actions_df.to_csv('new_at.csv')
     new_actions_dict = new_actions_df.to_dict(orient='records')
 
-    for test in new_actions_dict:
-        test2 = {k: v for k, v in test.items() if v}
-        bp_actions_tbl.insert(test2, typecast=True)
+    for action in new_actions_dict:
+        action2 = {k: v for k, v in action.items() if v}
+        bp_actions_tbl.insert(action2, typecast=True)
     print ('{} rows added'.format(new_rows))
